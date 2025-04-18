e36f270c45dc45ac69adc5fd5d77a1fa33934a60
1l8c3 <131458871+1l8c3@users.noreply.github.com>
2025-04-18 19:46:20 +0200

    suite

---
+++ src/Modele/Marque.java
@@ -0,0 +1,31 @@
+package Modele;
+
+public class Marque {
+    private int idMarque;
+    private String nom;
+
+    public Marque() {}
+
+    public Marque(int idMarque, String nom) {
+        this.idMarque = idMarque;
+        this.nom = nom;
+    }
+
+    public int getIdMarque() {
+        return idMarque;
+    }
+    public void setIdMarque(int idMarque) {
+        this.idMarque = idMarque;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
